fc0d2b15ab72d6d842a42115b86254957fe310b0
Wilder Taype <i201418741@cibertec.edu.pe>
2026-06-23 14:51:02 -0500

    Actualizacion Principal v33.10.10

--- smiles/wii.js
+++ smiles/wii.js
@@ -10,15 +10,15 @@ export let lanzamiento = 2026;
 export let by = '@wilder.taype';
 export let linkme = 'https://wtaype.github.io/';
 export let ipdev = import.meta.env.VITE_DEV;
-export let version = '34';
+export let version = 'v33';
 
 /** ACTUALIZAR AL TAG POR SEGURIDAD [TAG NUEVO] (1)
-git tag 34 -m "Version 34" ; git push origin 34
+git tag v33 -m "Version v33" ; git push origin v33
 
 ACTUALIZACIÓN AL MAIN PRINCIPAL DEL PROYECTO [MAIN] (2)
-git add . ; git commit -m "Actualizacion Principal 34.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v33.10.10" ; git push origin main
 
 // REEMPLAZAR TAG DE SEGURIDAD EXISTENTE [TAG REMPLAZO] (3)
-git tag -d 34 ; git tag 34 -m "Version 34 actualizada" ; git push origin 34 --force
+git tag -d v33 ; git tag v33 -m "Version v33 actualizada" ; git push origin v33 --force
 
  ACTUALIZACION TAG[END] */
